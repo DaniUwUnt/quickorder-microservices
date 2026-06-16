66c31da74228e69f691b397484f388e2a331ae55
Daniela Salinas <da.salinas@duocuc.cl>
2026-06-16 19:52:01 -0400

    feat: agrega Eureka Server y API Gateway

---
+++ api-gateway/src/test/java/com/quickorder/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickorder.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
